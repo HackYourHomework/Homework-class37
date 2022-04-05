2a32fc4da98cbbc9c61e2806a6ec28fb2fa4718e
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-04-05 11:25:46 +0200

    Yasemin Caliskan  JavaScript Week-3 Exercise-1 done!

--- 1-JavaScript/Week3/homework/ex1-giveCompliment.js
+++ 1-JavaScript/Week3/homework/ex1-giveCompliment.js
@@ -18,13 +18,16 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-Java
    Use `console.log` each time to display the return value of the
    `giveCompliment` function to the console.
 -----------------------------------------------------------------------------*/
-function giveCompliment(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function giveCompliment(name){
+const compliments=['great','perfect','awesome','incredible','the best','magnificient','stunning','genious','smart','nice'];
+let currentIndex= Math.floor(Math.random()*10);
+ return `You are ${compliments[currentIndex]}, ${name}!`;
 }
+
 
 function main() {
   // TODO substitute your own name for "HackYourFuture"
-  const myName = 'HackYourFuture';
+  const myName = 'Yasemin';
 
   console.log(giveCompliment(myName));
   console.log(giveCompliment(myName));

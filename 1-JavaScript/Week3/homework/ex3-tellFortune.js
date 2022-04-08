3316d0809db94d30ecc8376c0b93bea03645ede0
Amr-Yehia <101204943+Amr-Yehia@users.noreply.github.com>
2022-04-08 23:49:09 +0200

    gived 5 values to arrays and still working on the rest

--- 1-JavaScript/Week3/homework/ex3-tellFortune.js
+++ 1-JavaScript/Week3/homework/ex3-tellFortune.js
@@ -1,31 +1,24 @@
 'use strict';
 /*------------------------------------------------------------------------------
 Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-JavaScript/Week3#exercise-3-be-your-own-fortune-teller
-
 Why pay a fortune teller when you can just program your fortune yourself?
-
 1. Create four arrays, `numKids`, `partnerNames`, `locations` and `jobTitles`. 
    Give each array five random values that have to do with the name of 
    the variable.
-
 2. Complete the function `selectRandomly`. This function should take an array 
    as a parameter and return a randomly selected element as its return value.
-
-3. Complete the function named `tellFortune` as follows:
-
-   - It should take four arguments (in the order listed): 
-     * the array with the options for the number of children, 
-     * the array with the options for the partner's name, 
-     * the array with the options for the geographic location and 
-     * the array with the options for the job title.
-   - It should use the `selectRandomly` function to randomly select values from 
-     the arrays.
-   - It should return a string: "You will be a `jobTitle` in `location`, 
-    married to `partnerName` with `numKids` kids."
-
+// 3. Complete the function named `tellFortune` as follows:
+//    - It should take four arguments (in the order listed): 
+//      * the array with the options for the number of children, 
+//      * the array with the options for the partner's name, 
+//      * the array with the options for the geographic location and 
+//      * the array with the options for the job title.
+//    - It should use the `selectRandomly` function to randomly select values from 
+//      the arrays.
+//    - It should return a string: "You will be a `jobTitle` in `location`, 
+//     married to `partnerName` with `numKids` kids."
 4. Call the function three times, passing the arrays as arguments. Use `
    console.log` to display the results.
-
 Note: The DRY principle is put into practice here: instead of repeating the code to 
 randomly select array elements four times inside the `tellFortune` function 
 body, this code is now written once only in a separated function.
@@ -33,29 +26,69 @@ body, this code is now written once only in a separated function.
 
 // This function should take an array as its parameter and return
 // a randomly selected element as its return value.
-function selectRandomly(/* TODO parameter(s) go here */) {
+const values = [254, 45, 212, 365, 2543];
+let value = values[Math.floor(Math.random() * values.length)];
+function selectRandomly(value) {
   // TODO complete this function
+  
+  return value
+    
 }
 
-function tellFortune(/* TODO add parameter(s) here */) {
+// Complete the function named `tellFortune` as follows:
+//    - It should take four arguments (in the order listed): 
+//      * the array with the options for the number of children, 
+//      * the array with the options for the partner's name, 
+//      * the array with the options for the geographic location and 
+//      * the array with the options for the job title.
+//    - It should use the `selectRandomly` function to randomly select values from 
+//      the arrays.
+//    - It should return a string: "You will be a `jobTitle` in `location`, 
+//     married to `partnerName` with `numKids` kids."
+function tellFortune() {
   // TODO complete this function
+  main();
+  const numKid = numKids[Math.floor(Math.random() * numKids.length)];
+ 
+
+
+
+
+
+  
 }
 
 function main() {
   const numKids = [
     // TODO add elements here
+    2, 6, 9, 5, 3
   ];
 
   const partnerNames = [
     // TODO add elements here
+    "Shaimaa",
+    "Samar",
+    "Aya",
+    "Mona",
+    "Hager"
   ];
 
   const locations = [
     // TODO add elements here
+    "Egypt",
+    "New York",
+    "Italy",
+    "Spain",
+    "Brazil"
   ];
 
   const jobTitles = [
     // TODO add elements here
+    "Web Developer",
+    "Software Developer",
+    "Business Analyst",
+    "IT Manager",
+    "Software Architect"
   ];
 
   console.log(tellFortune(numKids, partnerNames, locations, jobTitles));

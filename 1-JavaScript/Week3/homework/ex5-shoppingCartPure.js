68b6724b9802efc62daddb717f2e4f35570b95bb
Amr-Yehia <101204943+Amr-Yehia@users.noreply.github.com>
2022-04-11 16:09:24 +0200

    completed ex5-shoppingCartPure.js

--- 1-JavaScript/Week3/homework/ex5-shoppingCartPure.js
+++ 1-JavaScript/Week3/homework/ex5-shoppingCartPure.js
@@ -15,9 +15,27 @@ it pure. Do the following:
    spread syntax.
 5. Confirm that you function passes the provided unit tests.
 ------------------------------------------------------------------------------*/
+
+ 
 // ! Function under test
-function addToShoppingCart(/* TODO parameter(s) go here */) {
+function addToShoppingCart(cart, gro) {
   // TODO complete this function
+  const newArray = [...cart, gro];
+  
+  
+  if (newArray.length > 3){
+    return newArray.slice(newArray.length - 3);
+  }else{
+    return newArray;
+  }
+
+  // if(cart === 3){
+  //   addToShoppingCart();
+  // }else{
+  //   const newShoppingCart = initialCart.slice();
+  //   console.log(newShoppingCart);
+  // } 
+  
 }
 
 // ! Test functions (plain vanilla JavaScript)

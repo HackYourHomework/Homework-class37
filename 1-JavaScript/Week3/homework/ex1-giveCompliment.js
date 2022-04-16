5f347cf65bd4b0bcad6adc40244bf8af5969479a
Camille Schull <camilleschull@voormedia.com>
2022-04-16 09:05:40 +0200

    Cleaning up this code.

--- 1-JavaScript/Week3/homework/ex1-giveCompliment.js
+++ 1-JavaScript/Week3/homework/ex1-giveCompliment.js
@@ -4,11 +4,8 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-Java
 
 1. Complete the function named `giveCompliment`as follows:
 
-<<<<<<< HEAD
-   - It should take a single parameter: `name`. 
-=======
    - It should take a single parameter: `name`.
->>>>>>> 36f5667322659c92acb0b271ad9e082da648e18a
+   - It should take a single parameter: `name`.
    - Its function body should include a variable that holds an array,
      `compliments`, initialized with 10 strings. Each string should be a
      compliment, like `"great"`, `"awesome"` and so on.
@@ -27,23 +24,20 @@ function giveCompliment(name) {
   const compliments= ["great", "awesome","nice", "beautiful", "strong","powerful", "friendly", "luxury","sweet","perfect" ]
 
   const randomElement = compliments[Math.floor(Math.random() * compliments.length)];
-  
+
    return "You are " + randomElement + ", " + name +"!";
 
 }
 
 function main() {
- 
+
   const myName = 'Oscar';
-=======
-function giveCompliment(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+
 }
 
 function main() {
   // TODO substitute your own name for "HackYourFuture"
   const myName = 'HackYourFuture';
->>>>>>> 36f5667322659c92acb0b271ad9e082da648e18a
 
   console.log(giveCompliment(myName));
   console.log(giveCompliment(myName));

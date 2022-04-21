1a5e52adfeb65549a8693407548c7d287f2c934e
Serva <servas230@gmail.com>
2022-04-21 14:45:50 +0200

    Modified ex-3 to pass the test

--- 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
+++ 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
@@ -42,18 +42,21 @@ console.log(sanitizeFruitBasket(fruitBasket, 'lemon'));
 describe('sanitizeFruitBasket', () => {
   test('should take two parameters', () => {
     // TODO replace next line with your code
-    expect(false).toBe(true);
+
+    expect(sanitizeFruitBasket).toHaveLength(2);
   });
 
   test('should not modify the original `fruitBasket` array', () => {
     // Save the original contents of the fruit basket
     const originalFruitBasketContents = [...fruitBasket];
     // TODO replace next line with your code
-    expect(false).toBe(true);
+    expect(fruitBasket).toEqual(originalFruitBasketContents);
   });
 
   test('should return a new array that does not include the unwanted `lemon`', () => {
     // TODO replace next line with your code
-    expect(false).toBe(true);
+    expect(['apple', 'grapefruit', 'banana', 'watermelon']).toEqual(
+      sanitizeFruitBasket(fruitBasket, 'lemon')
+    );
   });
 });

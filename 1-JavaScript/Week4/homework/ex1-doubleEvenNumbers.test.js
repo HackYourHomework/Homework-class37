3c31344844c6a7ff4341bf2152bdfec66827ff5d
Serva <servas230@gmail.com>
2022-04-21 09:02:38 +0200

    modified ex-1

--- 1-JavaScript/Week4/homework/ex1-doubleEvenNumbers.test.js
+++ 1-JavaScript/Week4/homework/ex1-doubleEvenNumbers.test.js
@@ -39,6 +39,4 @@ test('doubleEvenNumbers should take the even numbers and double them', () => {
   const actual = doubleEvenNumbers([1, 2, 3, 4]);
   const expected = [4, 8];
   expect(actual).toEqual(expected);
-  console.log(actual);
-  console.log(expected);
 });

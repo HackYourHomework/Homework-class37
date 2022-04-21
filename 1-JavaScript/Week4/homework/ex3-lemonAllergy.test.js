35825792e08f061838f5868e45ca800570e97550
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-04-21 23:34:37 +0200

    sanitizeFruitBasket function refactored . It takes two parameters and test functions refactored

--- 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
+++ 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
@@ -26,26 +26,26 @@ const fruitBasket = [
 ];
 
 // ! Function under test
-function sanitizeFruitBasket(fruitBasket) {
-  return [...fruitBasket.filter((fruit) => fruit !== 'lemon')];
+function sanitizeFruitBasket(fruitBasket, fruitOut) {
+  return fruitBasket.filter((fruit) => fruit !== fruitOut);
 }
 
 // ! Unit tests (using Jest)
 describe('sanitizeFruitBasket', () => {
   test('should take two parameters', () => {
-    expect(sanitizeFruitBasket).toHaveLength(1);
+    expect(sanitizeFruitBasket).toHaveLength(2);
   });
 
   test('should not modify the original `fruitBasket` array', () => {
     // Save the original contents of the fruit basket
     const originalFruitBasketContents = [...fruitBasket];
     expect(originalFruitBasketContents).not.toEqual(
-      sanitizeFruitBasket(fruitBasket)
+      sanitizeFruitBasket(fruitBasket, 'lemon')
     );
   });
 
   test('should return a new array that does not include the unwanted `lemon`', () => {
     const expectedBasket = ['apple', 'grapefruit', 'banana', 'watermelon'];
-    expect(sanitizeFruitBasket(fruitBasket)).toEqual(expectedBasket);
+    expect(sanitizeFruitBasket(fruitBasket, 'lemon')).toEqual(expectedBasket);
   });
 });

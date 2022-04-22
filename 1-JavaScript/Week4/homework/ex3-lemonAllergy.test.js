b38f69b6d41ec3221edf22c0b6d0b1f9c7e5b7c3
mansoor.hamo <mans.hamo@gmail.com>
2022-04-22 18:45:26 +0200

    I completed ex3 and passd the test

--- 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
+++ 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
@@ -26,26 +26,31 @@ const fruitBasket = [
 ];
 
 // ! Function under test
-function sanitizeFruitBasket(/* TODO parameter(s) go here */) {
+function sanitizeFruitBasket(fruits, sanitizeFruit) {
   // TODO complete this function
+  const newFruitsBasket = fruits.filter((item) => item !== sanitizeFruit);
+  return newFruitsBasket;
 }
 
 // ! Unit tests (using Jest)
 describe('sanitizeFruitBasket', () => {
   test('should take two parameters', () => {
     // TODO replace next line with your code
-    expect(false).toBe(true);
+    expect(sanitizeFruitBasket).toHaveLength(2);
   });
 
   test('should not modify the original `fruitBasket` array', () => {
     // Save the original contents of the fruit basket
     const originalFruitBasketContents = [...fruitBasket];
+    const result = fruitBasket;
     // TODO replace next line with your code
-    expect(false).toBe(true);
+    expect(result).toEqual(originalFruitBasketContents);
   });
 
   test('should return a new array that does not include the unwanted `lemon`', () => {
     // TODO replace next line with your code
-    expect(false).toBe(true);
+    const result = sanitizeFruitBasket(fruitBasket, 'lemon');
+    const expected = ['apple', 'grapefruit', 'banana', 'watermelon'];
+    expect(result).toEqual(expected);
   });
 });

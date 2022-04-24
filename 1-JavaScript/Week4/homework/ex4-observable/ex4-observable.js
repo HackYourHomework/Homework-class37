693dd5bded3127b342edfac25c1b226661554833
Mansoor Hamozadeh <99452509+mans-a20@users.noreply.github.com>
2022-04-24 18:18:47 +0200

    Update ex4-observable.js
    I renamed the  item

--- 1-JavaScript/Week4/homework/ex4-observable/ex4-observable.js
+++ 1-JavaScript/Week4/homework/ex4-observable/ex4-observable.js
@@ -18,11 +18,9 @@ function createObservable() {
   return {
     subscribe: function (subscriber) {
       subscribers.push(subscriber);
-      // TODO complete this function
     },
     notify: function (message) {
-      // TODO complete this function
-      subscribers.forEach((item) => item(message));
+      subscribers.forEach((subscriber) => subscriber(message));
     },
   };
 }

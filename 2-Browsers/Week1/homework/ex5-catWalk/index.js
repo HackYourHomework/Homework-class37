e801866aab628a0e383068c28914e08012ea4ed5
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-05-03 00:51:04 +0200

    exercise 5 code is made more readable

--- 2-Browsers/Week1/homework/ex5-catWalk/index.js
+++ 2-Browsers/Week1/homework/ex5-catWalk/index.js
@@ -3,27 +3,25 @@
 function catWalk() {
   const imgEl = document.querySelector('img');
   const currentLeft = parseInt(imgEl.style.left);
+  const middleOfTheScreen = (window.innerWidth - imgEl.width) / 2;
+  const rightHandOfTheScreen = window.innerWidth - imgEl.width;
+  const dancingCat =
+    'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+  const walkingCat = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
 
-  if (
-    imgEl.style.left === '' ||
-    currentLeft > window.innerWidth - imgEl.width
-  ) {
+  if (imgEl.style.left === '' || currentLeft > rightHandOfTheScreen) {
     imgEl.style.left = '0px';
-  } else if (
-    imgEl.src ===
-    'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif'
-  ) {
+  } else if (imgEl.src === dancingCat) {
     imgEl.style.left = currentLeft + 'px';
   } else {
     imgEl.style.left = currentLeft + 10 + 'px';
   }
 
-  if (currentLeft === Math.trunc((window.innerWidth - imgEl.width) / 20) * 10) {
-    imgEl.src =
-      'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+  if (currentLeft === Math.trunc(middleOfTheScreen / 10) * 10) {
+    imgEl.src = dancingCat;
     imgEl.alt = 'Cat dancing';
     setTimeout(() => {
-      imgEl.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+      imgEl.src = walkingCat;
       imgEl.alt = 'Cat walking';
     }, 5000);
   }

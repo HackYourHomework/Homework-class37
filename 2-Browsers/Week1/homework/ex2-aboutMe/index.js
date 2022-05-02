c756adb7df4ba651f934a3f8382c11bd0703d9f8
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-05-02 17:44:08 +0200

    exercise 2 todos are deleted

--- 2-Browsers/Week1/homework/ex2-aboutMe/index.js
+++ 2-Browsers/Week1/homework/ex2-aboutMe/index.js
@@ -1,13 +1,5 @@
 'use strict';
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Browsers/Week1#exercise-2-about-me
 
-1. Using JavaScript, replace each of the spans (`nickname`, fav-food`, 
-   `hometown`) with your own information.
-2. In JavaScript, iterate through each `<li>` and change the class to 
-   `list-item`.
-3. Look in the css file!
-------------------------------------------------------------------------------*/
 const bodyEl = document.querySelector('body');
 bodyEl.style.fontFamily = 'Arial, sans-serif';
 const nickNameEl = document.querySelector('#nickname');

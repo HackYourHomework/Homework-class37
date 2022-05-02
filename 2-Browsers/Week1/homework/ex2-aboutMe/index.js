7455c3b00836d77772803e8418ede8a3ab619677
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-05-02 16:26:28 +0200

    exercise 2 is done successfully . Body element is selected and font syle changed

--- 2-Browsers/Week1/homework/ex2-aboutMe/index.js
+++ 2-Browsers/Week1/homework/ex2-aboutMe/index.js
@@ -8,11 +8,13 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
    `list-item`.
 3. Look in the css file!
 ------------------------------------------------------------------------------*/
+const bodyEl = document.querySelector('body');
+bodyEl.style.fontFamily = 'Arial, sans-serif';
 const nickNameEl = document.querySelector('#nickname');
 nickNameEl.textContent = 'Yasemin';
 const favFoodEl = document.querySelector('#fav-food');
 favFoodEl.textContent = 'Hamsi Tava';
-const homeTown = document.querySelector('#hometown');
-homeTown.textContent = 'Izmit';
+const homeTownEl = document.querySelector('#hometown');
+homeTownEl.textContent = 'Izmit';
 const liEls = document.querySelectorAll('li');
 liEls.forEach((element) => element.classList.add('list-item'));

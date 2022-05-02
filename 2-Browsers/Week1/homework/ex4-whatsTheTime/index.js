2116cdd38690d200cf32f16e02a31436360ed888
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-05-03 01:33:46 +0200

    toLocaleTimeToString Method is used

--- 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
+++ 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
@@ -1,19 +1,11 @@
 'use strict';
 
-function checkTime(timeItem) {
-  return timeItem < 10 ? '0' + timeItem : timeItem;
-}
-
 function addCurrentTime() {
-  const today = new Date();
-  const hour = checkTime(today.getHours());
-  const minute = checkTime(today.getMinutes());
-  const second = checkTime(today.getSeconds());
   const timeEl = document.querySelector('#time');
   timeEl.style.fontFamily = 'fantasy';
   timeEl.style.textAlign = 'center';
   timeEl.style.fontSize = 'large';
-  timeEl.textContent = hour + ':' + minute + ':' + second;
+  timeEl.textContent = new Date().toLocaleTimeString('it-IT');
   console.log(timeEl.textContent);
 }
 setInterval(addCurrentTime, 1000);

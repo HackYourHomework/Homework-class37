894eb393ec26eb3d5678d95b847df24dcbd3963d
Ghufran <ghufranthabit@gmail.com>
2022-05-03 21:46:08 +0200

    added the cat walk function

--- 2-Browsers/Week1/homework/ex5-catWalk/index.js
+++ 2-Browsers/Week1/homework/ex5-catWalk/index.js
@@ -21,8 +21,34 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
 
    https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif
 -----------------------------------------------------------------------------*/
-function catWalk() {
-  // TODO complete this function
-}
 
-// TODO execute `catWalk` when the browser has completed loading the page
+function catWalk() {
+  const dancingCat =
+    'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+  const walkingCat = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+  const imgEl = document.querySelector('img');
+  const startLeft = parseInt(imgEl.style.left);
+  const middleOfScreen =
+    Math.round((window.innerWidth - imgEl.width) / 20) * 10;
+  const rightOfScreen = window.innerWidth - imgEl.width;
+
+  if (imgEl.style.left === '' || startLeft > rightOfScreen) {
+    imgEl.style.left = '0px';
+  } else if (imgEl.src === dancingCat) {
+    imgEl.style.left = startLeft + 'px';
+  } else {
+    imgEl.style.left = startLeft + 10 + 'px';
+  }
+
+  if (startLeft === middleOfScreen) {
+    imgEl.src = dancingCat;
+    imgEl.alt = 'Dancing cat';
+
+    setTimeout(() => {
+      imgEl.src = walkingCat;
+      imgEl.alt = 'Walking cat';
+    }, 5000);
+  }
+}
+setInterval(catWalk, 50);
+window.addEventListener('load', catWalk);

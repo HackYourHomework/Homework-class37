a700dbaac0069559d66985edc4a325a6a4ffb13c
mansoor.hamo <mans.hamo@gmail.com>
2022-05-03 18:36:44 +0200

    completed ex1

--- 2-Browsers/Week1/homework/ex1-bookList/index.js
+++ 2-Browsers/Week1/homework/ex1-bookList/index.js
@@ -18,7 +18,35 @@ https: //hyf-js2-week1-makeme-ex1-demo.herokuapp.com/
 //cspell: enable
 
 function createBookList(books) {
-  // TODO your code goes in here, return the ul element
+  const newUl = document.createElement('ul');
+
+  books.forEach((book) => {
+    const newP = document.createElement('p');
+    newP.textContent = `${book.title}- ${book.author}`;
+    const newLi = document.createElement('li');
+
+    !book.alreadyRead
+      ? (newLi.style.background = 'red')
+      : (newLi.style.background = 'green');
+
+    const img = document.createElement('img');
+    newLi.appendChild(newP);
+    newLi.appendChild(img);
+    newUl.appendChild(newLi);
+
+    if (book.title === 'The Design of Everyday Things') {
+      img.src = 'assets/the_design_of_everyday_things.jpg ';
+      img.alt = ' The Design of Everyday Things';
+    } else if (book.title === 'The Most Human Human') {
+      img.src = 'assets/the_most_human_human.jpg ';
+      img.alt = ' The Most Human Human';
+    } else {
+      img.src = 'assets/the_pragmatic_programmer.jpg';
+      img.alt = ' The Pragmatic Programmer';
+    }
+    return newUl;
+  });
+  return newUl;
 }
 
 function main() {

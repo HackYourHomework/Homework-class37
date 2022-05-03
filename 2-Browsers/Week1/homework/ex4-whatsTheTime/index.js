ff4756a8b2de7c0bc4f31f7812b9c327e129c4a9
Ghufran <ghufranthabit@gmail.com>
2022-05-03 21:45:10 +0200

    added the function body

--- 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
+++ 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
@@ -1,4 +1,7 @@
 'use strict';
+
+const { time } = require('systeminformation');
+
 /*------------------------------------------------------------------------------
 Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Browsers/Week1#exercise-4-whats-the-time
 
@@ -8,7 +11,15 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
 2. Have the function execute when it's loading in the browser.
 ------------------------------------------------------------------------------*/
 function addCurrentTime() {
-  // TODO complete this function
+  const timeEl = new Date();
+  document.getElementById('time').textContent =
+    ('0' + timeEl.getHours()).slice(-2) +
+    ':' +
+    ('0' + timeEl.getMinutes()).slice(-2) +
+    ':' +
+    ('0' + timeEl.getSeconds() + 1).slice(-2);
 }
 
-// TODO execute `addCurrentTime` when the browser has completed loading the page
+window.onload = () => {
+  setInterval(addCurrentTime, 1000);
+};

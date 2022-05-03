e750e6b8d43351b819d5e2c272080f41f362ac2b
mansoor.hamo <mans.hamo@gmail.com>
2022-05-03 18:39:15 +0200

    completed ex2

--- 2-Browsers/Week1/homework/ex2-aboutMe/index.js
+++ 2-Browsers/Week1/homework/ex2-aboutMe/index.js
@@ -1,12 +1,19 @@
 'use strict';
-/*------------------------------------------------------------------------------
+/*--------------------------------st newLocal = 'Mansoor';----------------------------------------------
 Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Browsers/Week1#exercise-2-about-me
 
 1. Using JavaScript, replace each of the spans (`nickname`, fav-food`, 
    `hometown`) with your own information.
+   
 2. In JavaScript, iterate through each `<li>` and change the class to 
    `list-item`.
 3. Look in the css file!
 ------------------------------------------------------------------------------*/
 
-// TODO add your JavaScript code here.
+document.querySelector('#nickname').textContent = 'Mansoor';
+document.querySelector('#fav-food').textContent = 'Pizza';
+document.querySelector('#hometown').textContent = 'Heemskerk';
+document.body.style.fontFamily = 'Arial, sans-serif';
+document.querySelectorAll('li').forEach((item) => {
+  item.classList.add('list-item');
+});

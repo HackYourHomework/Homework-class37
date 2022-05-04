7c6ba5c4dc5a7271ff65d6be93161b72c34025ef
Fadi Naddaf <fady-naddaf@hotmail.com>
2022-05-04 17:44:47 +0200

    Not fully completed

--- 2-Browsers/Week1/homework/ex5-catWalk/index.js
+++ 2-Browsers/Week1/homework/ex5-catWalk/index.js
@@ -1,7 +1,6 @@
 'use strict';
 /*------------------------------------------------------------------------------
 Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Browsers/Week1#exercise-5-the-cat-walk
-
 1. Create a variable to store a reference to the `<img>` element.
 2. Change the style of the `<img>` to have a `left` of `0px`, so that it starts 
    at the left hand of the screen.
@@ -16,13 +15,41 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
    of a cat dancing (use this URL given below), keep it dancing for 5 seconds, 
    and then replace the img with the original image and have it 
    continue the walk.
-
    Dancing cat URL:
-
    https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif
 -----------------------------------------------------------------------------*/
-function catWalk() {
-  // TODO complete this function
+// const background = document.querySelector('body');
+// background.style.backgroundColor = 'rgb(84, 84, 84)';
+
+const image = document.querySelector('img');
+const catWidth = image.width;
+// image.setAttribute('id', 'cat-pic');
+const screenWidth = window.innerWidth;
+image.style.left = '0px';
+
+function catWalk(e) {
+  const image = document.querySelector('img');
+  image.style.left = e.screenX + 'px';
+
+  // catWalk();
 }
 
-// TODO execute `catWalk` when the browser has completed loading the page
+window.onload = function () {
+  catWalk();
+};
+// window.addEventListener('load', catWalk);
+
+// const background = document.querySelector('body');
+// background.style.backgroundColor = 'rgb(84, 84, 84)';
+
+// const image = document.querySelector('img');
+// image.style.left = '0px';
+// let catWidth = image.width;
+// const screenWidth = window.innerWidth;
+
+// function catWalk() {
+//   image.style.left = catWidth + 'px';
+//   catWalk();
+// }
+
+// window.addEventListener('load', catWalk);

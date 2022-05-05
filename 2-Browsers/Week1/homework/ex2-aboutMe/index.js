5be102ebdd22001325e5ed3201eca79a833c706d
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-05-05 11:28:28 +0200

    blank lines added

--- 2-Browsers/Week1/homework/ex2-aboutMe/index.js
+++ 2-Browsers/Week1/homework/ex2-aboutMe/index.js
@@ -2,11 +2,15 @@
 
 const bodyEl = document.querySelector('body');
 bodyEl.style.fontFamily = 'Arial, sans-serif';
+
 const nickNameEl = document.querySelector('#nickname');
 nickNameEl.textContent = 'Yasemin';
+
 const favFoodEl = document.querySelector('#fav-food');
 favFoodEl.textContent = 'Hamsi Tava';
+
 const homeTownEl = document.querySelector('#hometown');
 homeTownEl.textContent = 'Izmit';
+
 const liEls = document.querySelectorAll('li');
 liEls.forEach((element) => element.classList.add('list-item'));

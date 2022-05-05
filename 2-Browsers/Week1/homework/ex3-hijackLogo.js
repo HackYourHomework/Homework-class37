a33f42b0eafba65798fc576d6e155c3d05ef6397
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-05-05 11:30:31 +0200

    second src string is deleted

--- 2-Browsers/Week1/homework/ex3-hijackLogo.js
+++ 2-Browsers/Week1/homework/ex3-hijackLogo.js
@@ -3,8 +3,7 @@
 function hijackGoogleLogo() {
   const googleLogoEl = document.querySelector('.lnXdpd');
   googleLogoEl.src = 'https://www.hackyourfuture.dk/static/logo-dark.svg';
-  (googleLogoEl.srcset = 'https://www.hackyourfuture.dk/static/logo-dark.svg'),
-    'https://www.hackyourfuture.dk/static/logo-dark.svg';
+  googleLogoEl.srcset = 'https://www.hackyourfuture.dk/static/logo-dark.svg';
 }
 
 hijackGoogleLogo();

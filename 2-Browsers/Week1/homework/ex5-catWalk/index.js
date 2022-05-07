0c93365753adce6815d43405c3df91aa802d9822
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-05-07 20:08:09 +0200

    setInterval is edited

--- 2-Browsers/Week1/homework/ex5-catWalk/index.js
+++ 2-Browsers/Week1/homework/ex5-catWalk/index.js
@@ -1,34 +1,37 @@
 'use strict';
 const imgEl = document.querySelector('img');
+
 let currentLeft = 0;
+
 const dancingCat =
   'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+
 const walkingCat = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
 
 const middleOfTheScreen =
-  Math.round((window.innerWidth - imgEl.width) / 20) * 10;
+  Math.round((window.innerWidth - imgEl.width) / 2 / 10) * 10;
 
 const rightHandOfTheScreen = window.innerWidth - imgEl.width;
 
-let intervalId = setInterval(catWalk, 50);
-
 function catWalk() {
-  imgEl.style.left = `${currentLeft}px`;
-  currentLeft += 10;
+  const intervalId = setInterval(() => {
+    imgEl.style.left = `${currentLeft}px`;
+    currentLeft += 10;
 
-  if (currentLeft > rightHandOfTheScreen) {
-    currentLeft = 0;
-  } else if (currentLeft === middleOfTheScreen) {
-    imgEl.src = dancingCat;
-    imgEl.alt = 'Cat dancing';
-    clearInterval(intervalId);
+    if (currentLeft > rightHandOfTheScreen) {
+      currentLeft = 0;
+    } else if (currentLeft === middleOfTheScreen) {
+      imgEl.src = dancingCat;
+      imgEl.alt = 'Cat dancing';
+      clearInterval(intervalId);
 
-    setTimeout(() => {
-      imgEl.src = walkingCat;
-      imgEl.alt = 'Cat walking';
-      intervalId = setInterval(catWalk, 50);
-    }, 5000);
-  }
+      setTimeout(() => {
+        imgEl.src = walkingCat;
+        imgEl.alt = 'Cat walking';
+        catWalk();
+      }, 5000);
+    }
+  }, 50);
 }
 
 window.addEventListener('load', catWalk);

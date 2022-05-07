ff170143a988b468954c5499fe67f4006420343c
Ali GÖK <97063504+aligoknl@users.noreply.github.com>
2022-05-07 21:22:06 +0200

    ex5 variable name was changed to more readability

--- 2-Browsers/Week1/homework/ex5-catWalk/index.js
+++ 2-Browsers/Week1/homework/ex5-catWalk/index.js
@@ -21,26 +21,26 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
 
    https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif
 -----------------------------------------------------------------------------*/
-const cat = document.querySelector('img');
-let leftHand = 0;
+const catImage = document.querySelector('img');
+let leftHandOfScreen = 0;
 let walkSet = setInterval(catWalk, 50);
 
 function catWalk() {
 
-  leftHand += 10;
-  cat.style.left = `${leftHand}px`;
-  const screenAdjust = innerWidth - cat.width;
+  leftHandOfScreen += 10;
+  catImage.style.left = `${leftHandOfScreen}px`;
+  const screenAdjust = innerWidth - catImage.width;
 
-  if (leftHand === (screenAdjust) / 2 -(((screenAdjust) / 2) % 10)) {
+  if (leftHandOfScreen === (screenAdjust) / 2 -(((screenAdjust) / 2) % 10)) {
    setTimeout(() => {
-      cat.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+      catImage.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
       walkSet = setInterval(catWalk, 50);
     }, 5000);
 
     clearInterval(walkSet);
-    cat.src = 'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
-  } else if (leftHand > screenAdjust) {
-   leftHand = 0;
+    catImage.src = 'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+  } else if (leftHandOfScreen > screenAdjust) {
+   leftHandOfScreen = 0;
  }
 }
 

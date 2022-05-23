af4e39ab2230cf39d0de1d6f2ae813b78fe86fab
Ali GOK <aligok.nl@gmail.com>
2022-05-23 19:10:25 +0200

    ex2 solved

--- 3-UsingAPIs/Week1/homework/ex2-checkDoubleDigits.js
+++ 3-UsingAPIs/Week1/homework/ex2-checkDoubleDigits.js
@@ -12,8 +12,18 @@ Complete the function called `checkDoubleDigits` such that:
   "Expected a double digit number but got `number`", where `number` is the 
   number that was passed as an argument.
 ------------------------------------------------------------------------------*/
-function checkDoubleDigits(/* TODO add parameter(s) here */) {
-  // TODO complete this function
+function checkDoubleDigits(number) {
+  return new Promise((resolve, reject) => {
+    const minNumber = 9;
+    const maxNumber = 100;
+    if (number > minNumber && number < maxNumber) {
+      resolve('This is a double digit number!');
+    } else {
+      reject({
+        message: `Expected a double digit number but got ${number}.`,
+      });
+    }
+  });
 }
 
 function main() {

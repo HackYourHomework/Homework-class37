03e7de898776debd8645cb9c8715288c2562e73d
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-05-26 14:52:00 +0200

    at the end of the exercise, the question is replied

--- 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
+++ 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
@@ -16,3 +16,6 @@ if (process.env.NODE_ENV !== 'test') {
   main();
 }
 module.exports = rollDice;
+
+// JavaScript has the concept of "run to completion". Unless an error is thrown, a function is executed until a return statement or its end is reached.
+// In the case of a rejected promise, dice that have not yet finished their roll continue to do so since Javascript reached  neither return statement nor end.

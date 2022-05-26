31092a88819d2cb3ee81a1771c0242fa4060cfb2
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-05-26 14:19:23 +0200

    at the end of the exercise, question is replied inside commit

--- 3-UsingAPIs/Week1/homework/ex3-rollDie.js
+++ 3-UsingAPIs/Week1/homework/ex3-rollDie.js
@@ -41,3 +41,12 @@ if (process.env.NODE_ENV !== 'test') {
   main();
 }
 module.exports = rollDie;
+
+//The problem does not occur since the promise is settled with reject.
+// However in previous method with callback , if roll equals to randomrollstodo, the callback will be invoked again with the error value null.  Thus success message will be printed to console.
+// to avoid this situation  in callback version , if roll is greater than 6 the function can be returned immediately like this.
+/* if (roll === randomRollsToDo) {
+      callback(null, value);
+      return;
+    }
+    */

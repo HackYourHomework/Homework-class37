b17a2b6f1d3a0b489b0c549e1a73052260c9230b
tonyclem <tonyclem001@gmail.com>
2022-05-30 20:48:19 +0200

    CLEMENTSUNDAY Homework-Api-week2

--- 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
+++ 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 /*------------------------------------------------------------------------------
 Full description at: https://github.com/HackYourFuture/Homework/blob/main/3-UsingAPIs/Week2/README.md#exercise-1-programmer-fun
 
@@ -18,28 +19,44 @@ Full description at: https://github.com/HackYourFuture/Homework/blob/main/3-Usin
    should result in a network (DNS) error.
 ------------------------------------------------------------------------------*/
 function requestData(url) {
-  // TODO return a promise using `fetch()`
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(response.statusText);
+        }
+      })
+      .then((data) => resolve(data))
+      .catch((error) => reject(error.message));
+  });
 }
 
 function renderImage(data) {
-  // TODO render the image to the DOM
+  const elementImg = document.createElement('img');
+  elementImg.alt = 'image';
+  elementImg.src = data.img;
+  document.body.style.backgroundColor = 'green';
+  document.body.appendChild(elementImg);
   console.log(data);
 }
 
 function renderError(error) {
-  // TODO render the error to the DOM
+  const h1 = document.createElement('h1');
+  h1.textContent = error;
+  document.body.style.backgroundColor = 'red';
+  document.body.appendChild(h1);
   console.log(error);
 }
 
-// TODO refactor with async/await and try/catch
-function main() {
-  requestData('https://xkcd.now.sh/?comic=latest')
-    .then((data) => {
-      renderImage(data);
-    })
-    .catch((error) => {
-      renderError(error);
-    });
+async function main() {
+  try {
+    const data = await requestData('https://xkcd.now.sh/?comic=latest');
+    renderImage(data);
+  } catch (error) {
+    renderError(error);
+  }
 }
 
 window.addEventListener('load', main);

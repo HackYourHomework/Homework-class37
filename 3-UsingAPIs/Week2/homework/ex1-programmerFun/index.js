1252003e456f212496b85751246530a519bba2d2
Fadi <fady-naddaf@hotmail.com>
2022-06-01 00:26:40 +0200

    Adding the API week2 homework

--- 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
+++ 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
@@ -18,28 +18,35 @@ Full description at: https://github.com/HackYourFuture/Homework/blob/main/3-Usin
    should result in a network (DNS) error.
 ------------------------------------------------------------------------------*/
 function requestData(url) {
-  // TODO return a promise using `fetch()`
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => {
+      console.log(`Something went wrong ${error}`);
+    });
 }
 
 function renderImage(data) {
-  // TODO render the image to the DOM
-  console.log(data);
+  const image = document.createElement('img');
+  image.src = data.img;
+  image.alt = data.title;
+  document.body.appendChild(image);
+  console.log(image);
 }
 
 function renderError(error) {
-  // TODO render the error to the DOM
+  const errorMsg = document.createElement('h1');
+  errorMsg.textContent = `Something went wrong ${error}`;
+  document.body.appendChild(errorMsg);
   console.log(error);
 }
 
-// TODO refactor with async/await and try/catch
-function main() {
-  requestData('https://xkcd.now.sh/?comic=latest')
-    .then((data) => {
-      renderImage(data);
-    })
-    .catch((error) => {
-      renderError(error);
-    });
+async function main() {
+  try {
+    const jsonResponse = await requestData('https://xkcd.now.sh/?comic=latest');
+    renderImage(jsonResponse);
+  } catch (error) {
+    renderError(error);
+  }
 }
 
 window.addEventListener('load', main);

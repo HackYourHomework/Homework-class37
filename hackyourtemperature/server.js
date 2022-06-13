1f639d3d29fa9b9dd54ef1bf220d82a381793c81
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-06-13 17:06:37 +0200

    homework is done successfully

--- hackyourtemperature/server.js
+++ hackyourtemperature/server.js
@@ -1,19 +1,20 @@
 import express from 'express';
-const app = express();
-const router = express.Router();
 
+const app = express();
 const PORT = 3000;
-app.listen(PORT, () => {
-  console.log('server is running');
-});
+
 app.get('/', (req, res) => {
   res.send('Hello from backend to frontend');
 });
+
 app.use(express.json());
-//app.use(express.urlencoded({ extended: false }));
-console.log('hiep hiep hoorea');
+// app.use(express.urlencoded({ extended: false }));
 
 app.post('/weather', (req, res) => {
   const { cityName } = req.body;
   res.send(cityName);
+});
+
+app.listen(PORT, () => {
+  console.log('server is running');
 });
